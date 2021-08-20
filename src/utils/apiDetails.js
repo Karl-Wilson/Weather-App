ce92fd4dd30d5449ee7c9e45736aa5b5fdb3af47
karl-wilson <okosechidelu@gmail.com>
2021-08-20 17:59:04 +0100

    adjust

--- src/utils/apiDetails.js
+++ src/utils/apiDetails.js
@@ -1,7 +1,7 @@
 export const locationUrl = 'https://www.metaweather.com';
 export const locationSearch = '/api/location/search/?query=';
 export const locationData = '/api/location/';
-export const userIpUrl = 'http://api.ipify.org?format=json'
+export const userIpUrl = 'https://api.ipify.org?format=json'
 //export const geolocationUrl = 'http://api.ipstack.com/'
 //export const accessKey = 'fb03393153350bdc9583e7f1736094f0'
 export const geolocationUrl = 'https://api.ipgeolocation.io/ipgeo?'
--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,6 @@
 const fetchApi = async (url, data=false) => {
     const response = await fetch(url, {
         method: data? 'POST': 'GET',
-        mode: 'no-cors',
         body: data? data : null
     })
     return response.json()
